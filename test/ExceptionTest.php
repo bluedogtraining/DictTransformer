ccdc2b20e56f78cf164b66c55681b6aa2a8f6999
Johnson Page <jwpage@gmail.com>
2018-10-11 16:32:10 +1100

    Test additional exceptions

--- test/ExceptionTest.php
+++ test/ExceptionTest.php
@@ -11,9 +11,11 @@ use Bdt\DictTransformer\Resources\Item;
 use Test\Entities\MissingGetIdTile;
 use Test\Entities\Tile;
 
+use Test\Transformers\FieldTransformer;
 use Test\Transformers\Exceptions\MissingIncludeTransformer;
 use Test\Transformers\Exceptions\MissingTransformTransformer;
 use Test\Transformers\Exceptions\MissingGetIdTransformer;
+use Test\Transformers\Exceptions\InvalidIdTransformer;
 
 class ExceptionTest extends TestCase
 {
@@ -46,5 +48,21 @@ class ExceptionTest extends TestCase
         $tile = new MissingGetIdTile(1, 2);
 
         (new DictTransformer)->transform(new Item($tile, new MissingGetIdTransformer));
+    }
+    
+    /**
+     * @expectedException \Bdt\DictTransformer\Exceptions\InvalidResourceException
+     */
+    public function testInvalidResource()
+    {
+        (new DictTransformer)->transform(new InvalidResource([], new FieldTransformer));
+    }
+
+    /**
+     * @expectedException \Bdt\DictTransformer\Exceptions\InvalidIdException
+     */
+    public function testInvalidId()
+    {
+        (new DictTransformer)->transform(new Item([], new InvalidIdTransformer));
     }
 }
---
+++ test/InvalidResource.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Test;
+
+use Bdt\DictTransformer\Resources\AbstractResource;
+
+class InvalidResource extends AbstractResource 
+{
+}
